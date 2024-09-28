f7cf6d91bd750345b37318b7819eddf25556d9e1
prathix <ritukhanna1717@gmail.com>
2024-09-28 21:58:09 +0200

    added animation, and created database

--- bird_social/app/models.py
+++ bird_social/app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 40)
+    Description = models.CharField(max_length = 400)
+    image = models.ImageField(upload_to='app/images/')
