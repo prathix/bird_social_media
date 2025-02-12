bbdae116d5161cda4e0483a48074af5131243119
prathix <ritukhanna1717@gmail.com>
2025-02-12 21:21:13 +0100

    added login form

--- bird_social/app/urls.py
+++ bird_social/app/urls.py
@@ -8,5 +8,6 @@ app_name = "app"
 urlpatterns= [
     path('admin', admin.site.urls),
     path('', views.index, name='home'),
-    path('register', views.registration_form, name='register')
+    path('register', views.registration_form, name='register'),
+    path('login', views.login_form, name='login')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- bird_social/app/views.py
+++ bird_social/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Post
-from django.contrib .auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def index(request):
@@ -8,12 +9,21 @@ def index(request):
     return render(request, "app/index.html", {'posts': posts})
 
 def registration_form(request):
-
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            login(request, form.save())
             return redirect("/")
     else:
         form = UserCreationForm()
     return render(request, "app/auth.html", {"form": form})
+
+def login_form(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data = request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect("/")
+    else:
+        form = AuthenticationForm()
+    return render(request, "app/login.html", {"form": form})
